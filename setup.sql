37ad8619db6bc0fca051676e0da7b96f6b52883d
Sophie Marshall <sophie.marshall98@gmail.com>
2025-12-17 11:55:43 -0500

    Setup (#1)
    * some small things
    
    * setup infra

--- setup.sql
+++ setup.sql
@@ -1,8 +1,86 @@
--- account level object creation 
+/*-----------------------------------------------------------------------------
+Hands-On Lab: Data Engineering with Snowpark
+Script:       01_setup_snowflake.sql
+Author:       Jeremiah Hansen
+Last Updated: 1/1/2023
+-----------------------------------------------------------------------------*/
+
+
+-- ----------------------------------------------------------------------------
+-- Step #1: Accept Anaconda Terms & Conditions
+-- ----------------------------------------------------------------------------
+
+-- See Getting Started section in Third-Party Packages (https://docs.snowflake.com/en/developer-guide/udf/python/udf-python-packages.html#getting-started)
+
+
+-- ----------------------------------------------------------------------------
+-- Step #2: Create the account level objects
+-- ----------------------------------------------------------------------------
 USE ROLE ACCOUNTADMIN;
- 
--- roles 
+
+-- Roles
 SET MY_USER = CURRENT_USER();
-CREATE OR REPLCAE ROLE HOL_ROLE; 
-GRANT ROLE HOL_ROLW TO ROLE SYSADMIN; 
+CREATE OR REPLACE ROLE HOL_ROLE;
+GRANT ROLE HOL_ROLE TO ROLE SYSADMIN;
 GRANT ROLE HOL_ROLE TO USER IDENTIFIER($MY_USER);
+
+GRANT EXECUTE TASK ON ACCOUNT TO ROLE HOL_ROLE;
+GRANT MONITOR EXECUTION ON ACCOUNT TO ROLE HOL_ROLE;
+GRANT IMPORTED PRIVILEGES ON DATABASE SNOWFLAKE TO ROLE HOL_ROLE;
+
+-- Databases
+-- CREATE OR REPLACE DATABASE HOL_DB;
+GRANT OWNERSHIP ON DATABASE HOL_DB TO ROLE HOL_ROLE;
+
+-- Warehouses
+CREATE OR REPLACE WAREHOUSE HOL_WH WAREHOUSE_SIZE = XSMALL, AUTO_SUSPEND = 300, AUTO_RESUME= TRUE;
+GRANT OWNERSHIP ON WAREHOUSE HOL_WH TO ROLE HOL_ROLE;
+
+
+-- ----------------------------------------------------------------------------
+-- Step #3: Create the database level objects
+-- ----------------------------------------------------------------------------
+USE ROLE HOL_ROLE;
+USE WAREHOUSE HOL_WH;
+USE DATABASE HOL_DB;
+
+-- Schemas
+CREATE OR REPLACE SCHEMA EXTERNAL;
+CREATE OR REPLACE SCHEMA RAW_POS;
+CREATE OR REPLACE SCHEMA RAW_CUSTOMER;
+CREATE OR REPLACE SCHEMA HARMONIZED;
+CREATE OR REPLACE SCHEMA ANALYTICS;
+
+-- External Frostbyte objects
+-- use external schema to separate external stage and file format objects
+USE SCHEMA EXTERNAL;
+
+-- tell snowflake how to parse external files
+-- decouples ingestion from storage
+CREATE OR REPLACE FILE FORMAT PARQUET_FORMAT
+    TYPE = PARQUET
+    COMPRESSION = SNAPPY
+;
+
+-- create an external storage pointer that we can reuse later
+CREATE OR REPLACE STAGE FROSTBYTE_RAW_STAGE
+    URL = 's3://sfquickstarts/data-engineering-with-snowpark-python/'
+;
+
+-- ANALYTICS objects
+USE SCHEMA ANALYTICS;
+-- This will be added in step 5
+--CREATE OR REPLACE FUNCTION ANALYTICS.FAHRENHEIT_TO_CELSIUS_UDF(TEMP_F NUMBER(35,4))
+--RETURNS NUMBER(35,4)
+--AS
+--$$
+--    (temp_f - 32) * (5/9)
+--$$;
+
+-- define a SQL UDF taht we can use within our analytics layer
+CREATE OR REPLACE FUNCTION ANALYTICS.INCH_TO_MILLIMETER_UDF(INCH NUMBER(35,4))
+RETURNS NUMBER(35,4)
+    AS
+$$
+    inch * 25.4
+$$;
